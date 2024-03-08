a7e9455b66b43d5f25762f9d006f93e740bad1e7
Wisley Alves <wisley7lgithub@gmail.com>
2024-03-08 12:56:54 -0300

    chore(test): send Ip

---
+++ functions/routes/horus/test.js
@@ -0,0 +1,13 @@
+const axios = require('axios')
+
+exports.get = async ({ appSdk }, req, res) => {
+  const url = 'https://us-central1-horus-book-erp.cloudfunctions.net/app/horus/test'
+  const resp = await axios.get(url)
+  res.send(resp)
+}
+
+exports.post = ({ appSdk }, req, res) => {
+  console.log(req)
+  const ip = req.headers['x-forwarded-for'] || req.socket.remoteAddress
+  res.send({ ip })
+}
