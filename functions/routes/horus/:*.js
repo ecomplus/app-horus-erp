39de727fb5ab6a988f2ed7cc95544bf74ca289da
Wisley Alves <wisley7lgithub@gmail.com>
2024-03-08 17:15:21 -0300

    chore(proxy): method request is toLowerCase

--- functions/routes/horus/:*.js
+++ functions/routes/horus/:*.js
@@ -1,10 +1,10 @@
 const axios = require('axios')
 
 exports.all = async ({ appSdk }, req, res) => {
-  const { method, params, url, headers, body } = req
+  const { method, params, url, body } = req
   try {
     const urlRequest = params[0] + url.split(params[0])[1]
-    const { data } = await axios[method.toLowerCase()](urlRequest, { headers, body })
+    const { data } = await axios[method.toLowerCase()](urlRequest, { body })
     res.send(data)
   } catch (e) {
     console.error(e)
