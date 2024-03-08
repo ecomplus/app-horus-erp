7ddfbc5cdd0f35fc54f96fa34e97312111dc7292
Wisley Alves <wisley7lgithub@gmail.com>
2024-03-08 18:03:24 -0300

    chore(proxy): check authorization

--- functions/routes/horus/:*.js
+++ functions/routes/horus/:*.js
@@ -1,11 +1,14 @@
-const axios = require('axios')
+// const axios = require('axios')
 
 exports.all = async ({ appSdk }, req, res) => {
-  const { method, params, url, body } = req
+  const { method, params, url } = req
   try {
+    const authorization = req.headers.authorization
+    const [user, pass] = Buffer.from(authorization.replace('Basic ', ''), 'base64').toString('utf-8').split(':')
+
     const urlRequest = params[0] + url.split(params[0])[1]
-    const { data } = await axios[method.toLowerCase()](urlRequest, { body })
-    res.send(data)
+    // const { data } = await axios[method.toLowerCase()](urlRequest, { body, headers: { authorization } })
+    res.send({ user, pass, method, urlRequest })
   } catch (e) {
     console.error(e)
     throw e
