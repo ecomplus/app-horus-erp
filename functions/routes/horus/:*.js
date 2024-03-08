6b6020d54ad71894f9d9dd48297d263a13f2a12d
Wisley Alves <wisley7lgithub@gmail.com>
2024-03-08 18:09:18 -0300

    chore(proxy): send request to horus

--- functions/routes/horus/:*.js
+++ functions/routes/horus/:*.js
@@ -1,14 +1,15 @@
-// const axios = require('axios')
+const Horus = require('../../lib/horus/client')
 
 exports.all = async ({ appSdk }, req, res) => {
-  const { method, params, url } = req
+  const { method, params, url, body } = req
   try {
     const authorization = req.headers.authorization
     const [user, pass] = Buffer.from(authorization.replace('Basic ', ''), 'base64').toString('utf-8').split(':')
+    const horus = new Horus(null, user, pass)
 
     const urlRequest = params[0] + url.split(params[0])[1]
-    // const { data } = await axios[method.toLowerCase()](urlRequest, { body, headers: { authorization } })
-    res.send({ user, pass, method, urlRequest })
+    const { data } = horus[method.toLowerCase()](urlRequest, body)
+    res.send(data)
   } catch (e) {
     console.error(e)
     throw e
