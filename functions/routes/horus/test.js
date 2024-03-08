c9d01e3372083c83e0c67339b519d663b1d788ea
Wisley Alves <wisley7lgithub@gmail.com>
2024-03-08 13:42:18 -0300

    fix(test): new test ip send

--- functions/routes/horus/test.js
+++ functions/routes/horus/test.js
@@ -1,13 +1,7 @@
 const axios = require('axios')
 
 exports.get = async ({ appSdk }, req, res) => {
-  const url = 'https://webhook.site/08e6678c-cec8-4dde-a48d-6d1c0e61ae38'
-  const resp = await axios.get(url)
-  res.send(resp)
-}
-
-exports.post = ({ appSdk }, req, res) => {
-  console.log(req)
-  const ip = req.headers['x-forwarded-for'] || req.socket.remoteAddress
-  res.send({ ip })
+  const url = 'https://us-central1-test-app-ghanor.cloudfunctions.net/app/test'
+  const { data } = await axios.get(url)
+  res.send(data)
 }
