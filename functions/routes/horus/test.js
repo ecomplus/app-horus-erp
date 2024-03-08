620ddbc8cb0c6e9cebd55c2401ee1166d54ecb91
Wisley Alves <wisley7lgithub@gmail.com>
2024-03-08 15:47:39 -0300

    fix(test): check ip

--- functions/routes/horus/test.js
+++ functions/routes/horus/test.js
@@ -1,9 +1,10 @@
-const axios = require('axios')
+// const axios = require('axios')
 
 exports.get = async ({ appSdk }, req, res) => {
   try {
-    const url = 'curlmyip.org'
-    const { data } = await axios.get(url)
+    const data = 'ok'
+    // const url = https://curlmyip.org'
+    // const { data } = await axios.get(url)
     res.send(data)
   } catch (e) {
     console.error(e)
