6854c0d19dab44556d15b2a8e4cfee55a325fc83
Wisley Alves <wisley7lgithub@gmail.com>
2024-03-21 16:53:48 -0300

    fix(parser): fix parse date

--- functions/lib/parsers/date.js
+++ functions/lib/parsers/date.js
@@ -1,7 +1,7 @@
 const parseDate = (date = new Date(), isComplete) => {
   let format = `${date.getDay()}/${date.getMonth() + 1}/${date.getFullYear()}`
   if (isComplete) {
-    format += ` ${date.getHours}:${date.getMinutes()}:${date.getSeconds()}`
+    format += ` ${date.getHours()}:${date.getMinutes()}:${date.getSeconds()}`
   }
   return format
 }
