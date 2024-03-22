fe127ef8d32b67c279bc0cef41a032664bdaf5d3
Wisley Alves <wisley7lgithub@gmail.com>
2024-03-22 14:20:22 -0300

    fix(cron-events): add debbug

--- functions/lib/pub-sub/utils.js
+++ functions/lib/pub-sub/utils.js
@@ -33,12 +33,14 @@ const createEventsFunction = (
 
 const sendMessageTopic = async (eventName, json) => {
   const topicName = getPubSubTopic(eventName)
+  console.log('>> Send msg: ', eventName, ' ', JSON.stringify(json))
   return new PubSub()
     .topic(topicName)
     .publishMessage({ json })
     .then((messageId) => {
       console.log('>> MessageId: ', messageId, '-s', json?.storeId, ' Topic: ', topicName)
     })
+    .catch(err => console.error('E: ', err))
 }
 
 module.exports = {
