81015ad2c295be02445a227190ef8def13bbd15d
Wisley Alves <wisley7lgithub@gmail.com>
2024-03-25 19:30:21 -0300

    fix(event-product): init sync

--- functions/lib/cron-sync.js
+++ functions/lib/cron-sync.js
@@ -12,17 +12,20 @@ const { setup } = require('@ecomplus/application-sdk')
 // const requestHorus = require('./horus/request')
 // const { sendMessageTopic } = require('./pub-sub/utils')
 // syncCategory
-
+const collectionName = 'syncCategory'
 module.exports = context => setup(null, true, firestore())
   .then(async (appSdk) => {
+    console.log('>>init sync')
     const querySnapshot = await firestore()
-      .collection('syncCategory')
+      .collection(collectionName)
       .get()
 
+    console.log('>> querySnapshot ', querySnapshot)
     querySnapshot?.forEach(documentSnapshot => {
       const storeId = documentSnapshot.id
+      const docId = `${collectionName}/${storeId}`
       const data = documentSnapshot.data()
-      console.log('>> ', storeId, JSON.stringify(data))
+      console.log('>> ', storeId, JSON.stringify(data), docId)
     })
     return null
 
