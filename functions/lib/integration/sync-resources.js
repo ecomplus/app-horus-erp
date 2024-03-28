c544d47c26107c9e280759c3f7abf9b89753e3b0
Wisley Alves <wisley7lgithub@gmail.com>
2024-03-28 13:55:38 -0300

    fix(sync-resources): add sync resources

--- functions/lib/integration/sync-resources.js
+++ functions/lib/integration/sync-resources.js
@@ -1,8 +1,8 @@
 const { firestore } = require('firebase-admin')
 const { setup } = require('@ecomplus/application-sdk')
-const syncBrands = require('./handles-queues/sync-brands')
-const syncCategories = require('./handles-queues/sync-categories')
-const syncKits = require('./handles-queues/sync-kit')
+const syncBrands = require('./handles-queues/queue-brands')
+const syncCategories = require('./handles-queues/queue-categories')
+const syncKits = require('./handles-queues/queue-kit')
 
 module.exports = context => setup(null, true, firestore())
   .then(async (appSdk) => {
