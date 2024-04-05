283ee48e983785f7a4d4c00f397bb15e45c9209e
Wisley Alves <wisley7lgithub@gmail.com>
2024-04-04 22:05:40 -0300

    fix(import-all-products): test import products

--- functions/routes/horus/import-all-products.js
+++ functions/routes/horus/import-all-products.js
@@ -1,10 +1,60 @@
 const axios = require('axios')
+const getAppData = require('../../lib/zz/store-api/get-app-data')
+const {
+  collectionHorusEvents,
+  topicResourceToEcom
+} = require('./utils-variables')
+const { parseDate } = require('./parsers/parse-to-horus')
+const Horus = require('./horus/client')
+const requestHorus = require('./horus/request')
+const { sendMessageTopic } = require('./pub-sub/utils')
+
 const requestStoreApi = axios.create({
   baseURL: 'https://api.e-com.plus/v1',
   headers: {
     'Content-Type': 'application/json'
   }
 })
+
+// let hasRepeat = true
+// let offset = 0
+// const limit = 100
+
+// let total = 0
+// const promisesSendTopics = []
+// while (hasRepeat) {
+//   // create Object Horus to request api Horus
+//   const endpoint = `/Busca_Acervo${query}&offset=${offset}&limit=${limit}`
+//   const products = await requestHorus(horus, endpoint, 'get')
+//     .catch((err) => {
+//       if (err.response) {
+//         console.warn(JSON.stringify(err.response))
+//       } else {
+//         console.error(err)
+//       }
+//       return null
+//     })
+
+//   if (products && Array.isArray(products)) {
+//     total += products.length
+//     products.forEach((productHorus, index) => {
+//       promisesSendTopics.push(
+//         sendMessageTopic(
+//           topicResourceToEcom,
+//           {
+//             storeId,
+//             resource,
+//             objectHorus: productHorus,
+//             opts
+//           })
+//       )
+//     })
+//   } else {
+//     hasRepeat = false
+//   }
+
+//   offset += limit
+// }
 
 exports.post = async ({ appSdk }, req, res) => {
   const headers = req.headers
@@ -19,7 +69,7 @@ exports.post = async ({ appSdk }, req, res) => {
       let status = 400
       if (err.response) {
         status = err.response.status || status
-        message = err.response.data ? JSON.stringify(err.response.data) : message
+        // message = err.response.data ? JSON.stringify(err.response.data) : message
         console.warn(JSON.stringify(err.response))
       } else {
         console.error(err)
