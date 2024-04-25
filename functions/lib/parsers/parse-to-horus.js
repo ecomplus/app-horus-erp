62a0a0bd41d563dc33253355e25831eb7d1ae317
Wisley Alves <wisley7lgithub@gmail.com>
2024-04-25 15:42:09 -0300

    fix(cron): fix parse date

--- functions/lib/parsers/parse-to-horus.js
+++ functions/lib/parsers/parse-to-horus.js
@@ -8,7 +8,7 @@ const parseDate = (date = new Date(), isComplete) => {
   if (isComplete) {
     const hours = date.getHours()
     const minutes = date.getMinutes()
-    const seconds = '00' // date.getSeconds()
+    const seconds = 0 // date.getSeconds()
 
     format += ` ${hours < 10 ? `0${hours}` : hours}`
     format += `:${minutes < 10 ? `0${minutes}` : minutes}`
